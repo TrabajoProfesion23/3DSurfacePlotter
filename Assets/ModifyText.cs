933fb88cb08310ef4b470f142f3e300ac121083b
pagordm <pablo.gordillo@alum.unirioja.es>
2023-11-10 17:47:49 +0100

    Añadidos scripts de botones

---
+++ Assets/ModifyText.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ModifyText : MonoBehaviour
+{
+    private String expression;
+    // Start is called before the first frame update
+    void Start()
+    {
+        expression = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayExpression();
+    }
+
+    private void displayExpression()
+    {
+        TextMeshProUGUI tmp = this.gameObject.GetComponent<TextMeshProUGUI>();
+        String newexpr = "f(x,y)=" + expression.Replace("[y]", "y").Replace("[x]", "x");
+        tmp.SetText(newexpr);
+    }
+
+    public void addChar(string s)
+    {
+        expression += s;
+    }
+}
