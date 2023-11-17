e324e9df1d78a7537714e99059a85d12933b9b84
angutia <91556624+angutia@users.noreply.github.com>
2023-11-17 17:36:20 +0100

    Deformer

--- Assets/Deformer.cs
+++ Assets/Deformer.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using Unity.VisualScripting.Dependencies.NCalc;
 using Unity.VisualScripting;
 
+
 [RequireComponent(typeof(MeshFilter))]
 public class SingleThreadedDeformer : MonoBehaviour
 {
     [SerializeField] protected float _speed = 2.0f;
     [SerializeField] protected float _amplitude = 0.25f;
+    [SerializeField] protected GameObject text;
     protected Mesh Mesh;
 
     protected void Awake()
@@ -30,7 +32,8 @@ public class SingleThreadedDeformer : MonoBehaviour
         for (var i = 0; i < _vertices.Length; i++)
         {
             var position = _vertices[i];
-            position.y = DeformerUtilities.CalculateDisplacement(position, Time.time, _speed, _amplitude);
+            string exp = text.GetComponent<ModifyText>().getExpression();
+            position.y = DeformerUtilities.CalculateDisplacement(position, Time.time, _speed, _amplitude, exp);
             _vertices[i] = position;
         }
 
@@ -46,9 +49,10 @@ public class SingleThreadedDeformer : MonoBehaviour
 public static class DeformerUtilities
 {
     //[BurstCompile]
-    public static float CalculateDisplacement(Vector3 position, float time, float speed, float amplitude)
+    public static float CalculateDisplacement(Vector3 position, float time, float speed, float amplitude, string exp)
     {
-        Expression e = new Expression("Sqrt(Pow(10000,2)-Pow([x],2)-Pow([z],2))"); //Pow(1,2)-Pow([x],2)-Pow([z],2)
+        Expression e = new Expression(exp);
+        //Expression e = new Expression("Sqrt(Pow(10000,2)-Pow([x],2)-Pow([z],2))"); //Pow(1,2)-Pow([x],2)-Pow([z],2)
 
         e.Parameters["x"]=position.x;
         e.Parameters["z"]=position.z;
